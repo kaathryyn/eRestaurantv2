7ed41fe3cbf9e28a5370d0969ec05710ab880213
ishitaverghese26 <62224079+ishitaverghese26@users.noreply.github.com>
2020-04-23 15:16:05 +1000

    CustomerLogin
    Username and Password TextFields

--- client/src/Components/Login/login.js
+++ client/src/Components/Login/login.js
@@ -1,13 +1,49 @@
-import React, {Component} from 'react';
+import {React, useState, Component} from "react";
+import { Button, FormGroup, FormControl, ControlLabel } from "react-bootstrap";
+import "./Login.css";
+import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
+import {AppBar, TextField, RaisedButton}  from "material-ui/core";
 
-class Login extends Component {
+
+  class Login extends Component {
+    constructor(props){
+      super(props);
+      this.state={
+      username:'',
+      password:''
+      }
+    }
+
   render() {
-    return (
+      return (
         <div>
-          <h2>Log in to your account</h2>
-        </div>
+        <MuiThemeProvider>
+          <div>
+          <AppBar
+             title="Customer Login"
+           />
+           <TextField
+             hintText="Enter your Username"
+             floatingLabelText="Username"
+             onChange = {(event,newValue) => this.setState({username:newValue})}
+             />
+           <br/>
+             <TextField
+               type="password"
+               hintText="Enter your Password"
+               floatingLabelText="Password"
+               onChange = {(event,newValue) => this.setState({password:newValue})}
+               />
+             <br/>
+             <RaisedButton label="Submit" primary={true} style={style} onClick={(event) => this.handleClick(event)}/>
+         </div>
+         </MuiThemeProvider>
+      </div>
     );
   }
 }
-
+const style = {
+ margin: 15,
+};
 export default Login;
+       
