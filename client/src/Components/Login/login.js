0bc2b9d44753ebc4ec7d13c756f5670c77110a45
ishitaverghese26 <62224079+ishitaverghese26@users.noreply.github.com>
2020-05-25 17:50:59 +1000

    Login finally works!

--- client/src/Components/Login/login.js
+++ client/src/Components/Login/login.js
@@ -1,49 +1,89 @@
-import {React, useState, Component} from "react";
-import { Button, FormGroup, FormControl, ControlLabel } from "react-bootstrap";
-import "./Login.css";
-import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
-import {AppBar, TextField, RaisedButton}  from "material-ui/core";
+import React, {useState, Component} from 'react';
+import "./login.css";
+// import { createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles';
+// import TextField from "@material-ui/core/TextField";
+//import AppBar from "@material-ui/core/AppBar";
+import button from "@material-ui/core/Button";
+import {Card, Box} from "@material-ui/core";
+// import { auth } from '../../config/firebase.js';
+import firebase from '../../config/firebase';
+import right_image from '../../Images/Login.jpg';
 
 
   class Login extends Component {
     constructor(props){
       super(props);
       this.state={
-      username:'',
-      password:''
-      }
+      email:"",
+      password:""
+      
+    }; 
+
+    
+      this.handleSubmit=this.handleSubmit.bind(this) //this method is used to bind the handleSubmit operation with the constructor 
+
+  }
+
+    emailhandler = (event) => { //gather data from the username input field 
+      this.setState( { //user provides input in the heads and hit empty 
+        email: event.target.value
+      })
     }
 
+    passwordhandler = (event) => { //gather input data from the password field
+      this.setState( { //user provides input in the heads and hit empty 
+        password: event.target.value
+      })
+    }
+
+    handleSubmit = (event) => { //storing the state when the user provides data 
+      event.preventDefault();
+
+    firebase.auth().signInWithEmailAndPassword(this.state.email, this.state.password).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // [START_EXCLUDE]
+        if (errorCode === 'auth/wrong-password') {
+          alert('Wrong password.');
+        } else {
+          alert(errorMessage);
+        }
+        console.log(error);
+        // [END_EXCLUDE]
+      });
+      // [END authwithemail]
+    }
+      
+    
   render() {
-      return (
-        <div>
-        <MuiThemeProvider>
-          <div>
-          <AppBar
-             title="Customer Login"
-           />
-           <TextField
-             hintText="Enter your Username"
-             floatingLabelText="Username"
-             onChange = {(event,newValue) => this.setState({username:newValue})}
-             />
-           <br/>
-             <TextField
-               type="password"
-               hintText="Enter your Password"
-               floatingLabelText="Password"
-               onChange = {(event,newValue) => this.setState({password:newValue})}
-               />
-             <br/>
-             <RaisedButton label="Submit" primary={true} style={style} onClick={(event) => this.handleClick(event)}/>
-         </div>
-         </MuiThemeProvider>
-      </div>
-    );
-  }
+return (
+          <div className = "wrapper">
+            <Box className="main_box" variant="outlined">
+              <Box className = "toggle-inactive-label"></Box>
+            <h1 className="loginheader"> Log in </h1>
+              <Box className="details_box" variant="outlined">
+                  <h2 className="email"> Email </h2> 
+                  <input type="text" value={this.state.email}
+                  onChange={this.emailhandler}
+                  placeholder="Email"
+                  ></input> 
+                  <h2 className="password"> Password </h2> 
+                  <input type="text" value={this.state.password}
+                  onChange={this.passwordhandler}
+                  placeholder="6 digit"
+                  ></input> 
+                  <h6 className="forgot_password"> Forgot Password? Click here</h6>
+              </Box>
+            <button className="register_button" href = "registration.js"> Register </button>
+            <button onClick={this.handleSubmit} className="login_button"> Login </button>
+            </Box> 
+            <div className="right_image"></div>
+        </div>
+
+
+      );
+    }
 }
-const style = {
- margin: 15,
-};
 export default Login;
        
