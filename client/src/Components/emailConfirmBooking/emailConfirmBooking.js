282fbffb812b183be86524a6bf4c3de5a6ba27f9
jasminadang <jasminadang@gmail.com>
2020-05-26 21:07:58 +1000

    Email Booking Confirmation Page
    Created a new branch and completed the frontend for when users finished their booking and are taken to a new page stating that they will recieve an email confirmation.
    
    Will still need to do the backend and ensure the emails are working

---
+++ client/src/Components/emailConfirmBooking/emailConfirmBooking.js
@@ -0,0 +1,32 @@
+import React, {Component} from 'react';
+import "./emailConfirmBooking.css";
+import successTick from "../../Images/successTick.png"
+//import { auth } from '../../config/firebase.js';
+
+class EmailConfirmBooking extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+
+        }
+    }
+
+    render() {
+        return (
+        <div className = "success">
+        <img src = {successTick} className="greenTick" alt = "lock"/>
+        <h2>Order #12345 </h2>
+        <h2>Thanks for making a reservation with us!</h2>
+        <p>You'll soon receive an email from us to confirm your booking details.</p>
+        <p>Please not that if you've changed your mind on your order, you can always change this by asking our staff at the store.</p>
+        <button className = "homeButton" href = "home.js">Return to Home </button>
+        <button className = "editBooking"> Edit Booking</button>
+        </div>
+       )
+    }
+
+    
+}
+
+export default EmailConfirmBooking
