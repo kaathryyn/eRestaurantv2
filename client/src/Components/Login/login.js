a915a00ab896d36472d31f8971feff2710b2f4be
kaathryyn <48712975+kaathryyn@users.noreply.github.com>
2020-05-31 18:43:00 +1000

    Revert "Minor UI and link updates"

--- client/src/Components/Login/login.js
+++ client/src/Components/Login/login.js
@@ -51,8 +51,10 @@ class Login extends Component {
 
   handleSubmit = (event) => { //storing the state when the user provides data 
     event.preventDefault();
+    // this.props.logIn(this.state);
+
     firebase.auth().signInWithEmailAndPassword(this.state.email, this.state.password).then(() => {
-      window.location = '/';
+      window.location = 'menu';
     })
 
     // [END authwithemail]
@@ -82,6 +84,7 @@ class Login extends Component {
     const { authError } = this.props;
 
     return (
+
       <Grid container direction="row"  >
         <Grid item xs={6} justify="center" alignItems="center" >
           {/* <Box className="main_box" variant="outlined"> */}
@@ -114,8 +117,9 @@ class Login extends Component {
               value={this.state.password}
               onChange={this.passwordhandler}
             />
-            <a href="../ForgotPassword/ForgotPassword.js" className="forgot_password"> Forgot Password? Click here</a>
-            <a href="../Registration/registration.js"><button className="register_button" > Register </button></a>
+            <h6 className="forgot_password"> Forgot Password? Click here</h6>
+
+            <button className="register_button" > Register </button>
             <button onClick={this.handleSubmit} className="login_button" > Login </button>
             {/* </Box> */}
           </Card>
@@ -124,6 +128,7 @@ class Login extends Component {
           <img style={{ width: '100%', height: '100', position:' static' }} src={login} alt="login" />
         </Grid>
       </Grid>
+
     );
   }
 }
